5530ed536d1ebf5db1b2934b25aad104628c1ad6
jiekekeji <jieke_rose@foxmail.com>
2016-07-20 15:05:44 +0800

    mini

--- StudyAngularJS/src/main/java/com/mx/fm/controller/UserController.java
+++ StudyAngularJS/src/main/java/com/mx/fm/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.mx.fm.controller;
 
+import com.mx.fm.model.User;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +22,28 @@ public class UserController {
 
     private static Logger logger = Logger.getLogger(UserController.class);
 
-    @RequestMapping(value = "/checkUserIsExit", method = RequestMethod.GET)
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
     @ResponseBody
-    public Map checkUserIsExit(String username) {
-        logger.debug("checkUserIsExit=" + username);
-        return null;
+    public Map checkUserIsExit(String username,String password) {
+        logger.debug("register=" + username);
+        User user1=new User();
+        user1.setId(123);
+        user1.setUsername("jack");
+        user1.setPhonenum("18025232352");
+
+        User user2=new User();
+        user2.setId(321);
+        user2.setUsername("rose");
+        user2.setPhonenum("18532562535");
+
+        List<User> users=new ArrayList<User>();
+        users.add(user1);
+        users.add(user2);
+
+        Map map=new HashMap();
+        map.put("code",2000);
+        map.put("users",users);
+        return map;
     }
 
 
