432294d1752ed8b25721fee8eb199a4eb5769bc7
jiekekeji <jieke_rose@foxmail.com>
2016-08-17 00:03:18 +0800

    init

---
+++ StudyJQuery/js/demo004.js
@@ -0,0 +1,89 @@
+/**
+ * Created by jack on 16/8/16.
+ */
+//对象实例
+
+// 第一种方式
+var obj1 = new Object();
+obj1.name = "小明";
+obj1.age = 12;
+obj1.sex = "男";
+console.log("obj1 name=" + obj1.name);
+console.log("obj1 age=" + obj1.age);
+console.log("obj1 sex=" + obj1.sex);
+
+// 第二种方式,就是一个json格式的字符串
+var obj2 = {
+    "name": "小明",
+    "age": 12,
+    "sex": "男"
+}
+console.log("obj2 name=" + obj2.name);
+console.log("obj2 age=" + obj2.age);
+console.log("obj2 sex=" + obj2.sex);
+
+//大一点的对象
+var obj3 = {
+    "status": true,
+    "tngou": [{
+        "description": "性感美女",
+        "id": 1,
+        "keywords": "性感美女",
+        "name": "性感美女",
+        "seq": 1,
+        "title": "性感美女"
+    }, {
+        "description": "韩日美女",
+        "id": 2,
+        "keywords": "韩日美女",
+        "name": "韩日美女",
+        "seq": 2,
+        "title": "韩日美女"
+    }, {
+        "description": "丝袜美腿",
+        "id": 3,
+        "keywords": "丝袜美腿",
+        "name": "丝袜美腿",
+        "seq": 3,
+        "title": "丝袜美腿"
+    }, {
+        "description": "美女照片",
+        "id": 4,
+        "keywords": "美女照片",
+        "name": "美女照片",
+        "seq": 4,
+        "title": "美女照片"
+    }, {
+        "description": "美女写真",
+        "id": 5,
+        "keywords": "美女写真",
+        "name": "美女写真",
+        "seq": 5,
+        "title": "美女写真"
+    }, {
+        "description": "清纯美女",
+        "id": 6,
+        "keywords": "清纯美女",
+        "name": "清纯美女",
+        "seq": 6,
+        "title": "清纯美女"
+    }, {
+        "description": "性感车模",
+        "id": 7, "keywords": "性感车模",
+        "name": "性感车模", "seq": 7,
+        "title": "性感车模"
+    }]
+}
+
+console.log("obj3 status=" + obj3.status);
+console.log("obj3 tngou=" + obj3.tngou);
+// 遍历数组
+for(var i=0;i<obj3.tngou.length;i++){
+    console.log("*************");
+    var obj=obj3.tngou[i];
+    console.log(i+"obj description="+obj.description);
+    console.log(i+"obj id="+obj.id);
+    console.log(i+"obj keywords="+obj.keywords);
+    console.log(i+"obj name="+obj.name);
+    console.log(i+"obj title="+obj.title);
+}
---
+++ StudyJQuery/js/demo005.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jack on 16/8/16.
+ */
+//文档对象模型
+
+// document对象
+
+console.log("document="+document);
+// 获取元素
