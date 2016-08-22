bf098a0263d039e5c810339e73c917b6beb114e2
jiekekeji <jieke_rose@foxmail.com>
2016-08-22 22:44:45 +0800

    移动设备Web网页适配方案

--- MoblieProject001/js/demo002.js
+++ MoblieProject001/js/demo002.js
@@ -1,20 +1,21 @@
 /**
- * Created by jack on 16/8/21.
+ * 用viewport的initial-scale缩放页面
+ *
+ * 通过<meta name="viewport" content="width=320, initial-scale=1, maximum-scale=10, user-scalable=no">
+ * width设置为一个初始值,一般按设计图的倍数;比如设计图是640的,设置width=320,那么设计图上所有的尺寸都除以2,
+ * 页面初始加载时通过设备的宽度clientWidth和320的比值计算;initial-scale的比值.页面加载时就有相应的缩放.
  */
-
-
-$(document).ready(function () {
+function reSetInitialScale(){
     var html = document.documentElement;
     var windowWidth = html.clientWidth;
     var scale = windowWidth / 320;
-    console.log("scale=" + scale);
     var content = "width=320,, initial-scale=" + scale + ", maximum-scale=10, user-scalable=no";
     var viewport = $("meta[name='viewport']");
     viewport.attr("content", content);
-
-    $("#btn").click(function () {
-        console.log("div01 width=" + $(".div01").width());
-        var viewport = $("meta[name='viewport']");
-        console.log("content=" + viewport.attr("content"));
+}
+$(document).ready(function () {
+    reSetInitialScale();
+    $(window).resize(function() {
+        // reSetInitialScale();
     });
 });
