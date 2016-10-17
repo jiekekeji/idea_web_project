c56a309a93b8183b5940bbd78698a73539372ce0
jiekekeji <jieke_rose@foxmail.com>
2016-10-17 23:35:06 +0800

    简单的理解闭包

---
+++ MyWorks/mwk001/js/demo001.js
@@ -0,0 +1,37 @@
+/**
+ * Created by jack on 16/10/17.
+ */
+$(document).ready(function () {
+    console.log("加载完成");
+
+    var i = 0;//计数器
+    var interval;//定时任务
+
+    function initMainAD(count, width) {
+        $(".mainAD>ul").css("width", width * count + 50 + "px");
+        $(".mainAD>ul").hover(function () {
+            console.log("停止播放");
+                clearInterval(interval);
+            },
+            function () {
+                console.log("再次播放");
+                interval = setInterval(playAmin, 1000);
+            });
+    }
+
+    function playAmin() {
+        console.log("下一张");
+        if (i == 3) {
+            i = 0;
+        }
+        $(".mainAD>ul").animate({
+            left: -i * 1080 + "px"
+        });
+        i++;
+    }
+
+    initMainAD(3, 1080);
+    interval = setInterval(playAmin, 1000);
+
+
+});
