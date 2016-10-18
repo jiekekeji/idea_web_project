596200e8c1ae1f7b7b2a4c03e5c1f35c19e6e5b7
jiekekeji <jieke_rose@foxmail.com>
2016-10-18 10:16:14 +0800

    图片轮播修改

--- MyWorks/mwk001/js/demo001.js
+++ MyWorks/mwk001/js/demo001.js
@@ -2,36 +2,57 @@
  * Created by jack on 16/10/17.
  */
 $(document).ready(function () {
-    console.log("加载完成");
 
     var i = 0;//计数器
     var interval;//定时任务
+    var l2r = true;//从左向右滑动
+    var liCount = 0;//滚动图片的数量
+    var slideWidth = 0;//每次滚动的距离
 
-    function initMainAD(count, width) {
-        $(".mainAD>ul").css("width", width * count + 20 + "px");
+    function initMainAD(count, width, Interval) {
+        liCount = count;
+        slideWidth = width;
+        $(".mainAD>ul").css("width", liCount * slideWidth + 30 + "px");
+        //如果鼠标进入到滚动的区域就暂停滚动，离开则重新滚动
         $(".mainAD>ul").hover(function () {
-                console.log("停止播放");
                 clearInterval(interval);
             },
             function () {
-                console.log("再次播放");
                 interval = setInterval(playAmin, 1000 * 3);
             });
+        $(".mainAD>ul").scroll(function(event){
+            console.log("event="+event.width);
+        });
     }
 
+    /**
+     * 下一页的播放函数
+     */
     function playAmin() {
-        console.log("下一张");
-        if (i == 3) {
-            i = 0;
+        if (i == liCount - 1) {
+            l2r = false;//从右像左滚动
         }
+        if (i == 0) {
+            l2r = true;//从左向右滚动
+        }
+
         $(".mainAD>ul").animate({
             left: -i * 1080 + "px"
         }, 2 * 1000);
-        i++;
+
+        if (l2r) {
+            i++;
+        } else {
+            i--;
+        }
+
     }
 
     initMainAD(3, 1080);
     interval = setInterval(playAmin, 1000 * 3);
+
+    //var scrollbarWidth = $(".mainAD>ul").offsetWidth - $(".mainAD>ul").scrollWidth;
+    //console.log("滚动条的宽度:"+scrollbarWidth);
 
 
 });
