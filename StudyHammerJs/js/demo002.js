9678be8d960e73524441bf0ddbe108dd0c043e06
jiekekeji <jieke_rose@foxmail.com>
2016-10-19 23:06:16 +0800

    最简单图片轮播插件开发

--- StudyHammerJs/js/demo002.js
+++ StudyHammerJs/js/demo002.js
@@ -5,13 +5,17 @@ $(document).ready(function () {
     //初始化Hammer并传入需要操作的doc元素，不是jquery对象
     var content = new Hammer($(".content")[0]);
 
+    var leftMenuLeft=$(".leftMenu").css("left");
     //**********拖动事件*********************//
     content.on("pan", function (ev) {
         //这个偏移量是以当前元素的左上角顶点为准
         console.log("X偏移量:" + ev.deltaX);
         console.log("Y偏移量:" + ev.deltaY);
         if (ev.deltaX>0){
-            $(".leftMenu").css("left",ev.deltaX+"px");
+            console.log("leftMenuLeft="+leftMenuLeft+":"+leftMenuLeft+ev.deltaX);
+            leftMenuLeft=leftMenuLeft.substr(0,leftMenuLeft.length-2);
+            console.log("leftMenuLeft="+leftMenuLeft);
+            $(".leftMenu").css("left",leftMenuLeft+ev.deltaX+"px");
         }
 
 
