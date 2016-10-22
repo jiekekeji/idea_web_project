ddd2e93352a8a765bdc71a55da85321fb867f098
jiekekeji <jieke_rose@foxmail.com>
2016-10-22 11:29:08 +0800

    搞搞下拉刷新的东西

--- MyWorks/mwk002/js/demo001.js
+++ MyWorks/mwk002/js/demo001.js
@@ -3,6 +3,7 @@
  */
 $(document).ready(function () {
 
+
     var list = new Hammer($(".list")[0]);
     var startY = 0;
     var defaultMrTop=getOrgiListMrTop();
@@ -10,12 +11,21 @@ $(document).ready(function () {
     list.on("panstart", function (ev) {
         startY = ev.deltaY;
     })
-    //向下拖动
-    list.on("pandown", function (ev) {
-        var distance = ev.deltaY - startY;
+    list.on("pan", function (ev) {
+        console.log("startY="+startY);
+        console.log("ev.deltaY="+ev.deltaY);
+        console.log("defaultMrTop="+defaultMrTop);
+        var distance =ev.deltaY - startY-60;
         console.log("distance="+distance);
-        $(".content").css("margin-top",defaultMrTop+distance+"px");
-    })
+        $(".list").css("margin-top",(distance)+"px");
+    });
+
+    //向下拖动
+    //list.on("pandown", function (ev) {
+    //    var distance = ev.deltaY - startY;
+    //    console.log("distance="+distance);
+    //    $(".content").css("margin-top",defaultMrTop+distance+"px");
+    //})
     //拖动结束
     list.on("panend", function (ev) {
         console.log("拖动结束:" + "x=" + ev.deltaX + " y=" + ev.deltaY);
@@ -25,7 +35,7 @@ $(document).ready(function () {
      * 获取原始头部的margin-top距离
      */
     function  getOrgiListMrTop(){
-        var defaultMrTop = $(".content").css("margin-top");
+        var defaultMrTop = $(".list").css("margin-top");
         defaultMrTop=defaultMrTop.substr(0,defaultMrTop.length-2);
         return defaultMrTop;
     }
