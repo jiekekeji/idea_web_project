f8cca1acfa5f7e5ff232d8cbd54f9cfe4da3ce1c
jiekekeji <jieke_rose@foxmail.com>
2016-10-23 10:34:08 +0800

    冒泡

--- StudyJs/js/demo004.js
+++ StudyJs/js/demo004.js
@@ -2,13 +2,35 @@
  * Created by Administrator on 2016/10/14.
  */
 window.addEventListener("load", function () {
+    //嵌套型的
     $(".out").click(function () {
         console.log("out click");
     });
-    $(".inner").click(function () {
+    $(".inner").click(function (event) {
         console.log("inner click");
 
+        //阻止冒泡方法1
+        event.stopPropagation();
+        //阻止冒泡方法2
         //return false;阻断事件继续传递。就是不让它冒泡
-        return false;
+        // return false;
     });
+    //阻止默认事件,默认的a标签是会跳转的
+    $(".baidu").click(function (event) {
+        event.preventDefault();
+    });
+    //表单事件
+    $("#submit").click(function (event) {
+        var name = $(".other form input[name='name']").val();
+        var psw = $(".other form input[name='psw']").val();
+        console.log("name="+name+"&"+"psw="+psw);
+        if (name == "" || psw == "") {
+            //阻止事件提交
+            event.preventDefault();
+        }else {
+
+        }
+    });
+
+
 });
